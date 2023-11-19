e2cf96c34164a60998760ada25e953547312013f
YasarKantarcilar <kantarcilaryasar@gmail.com>
2023-11-19 15:15:02 +0300

    FEAT: tech stack animations

--- src/Components/TechStackSection.jsx
+++ src/Components/TechStackSection.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { MainContainer, TechStackInfoContainer, TechStackInfoText } from "./STYLED/Styled";
 import styled from "styled-components";
 import sticksToCircle from "../assets/Gifs/sticksToCircle.gif";
-
+import { motion } from "framer-motion";
+import "./STYLED/styles.css";
 const SticksGif = styled.img`
 	width: 30%;
 	height: 100%;
@@ -35,8 +36,18 @@ const TechStackSection = () => {
 		<MainContainer className="techStackCountainer" bg={"2"}>
 			<SticksGif src={sticksToCircle} />
 			<TechStackInfoContainer>
-				{myStack.map((item) => {
-					return <TechStackInfoText key={item}>{item}</TechStackInfoText>;
+				{myStack.map((item, index) => {
+					return (
+						<motion.div
+							initial={{ opacity: 0, x: -100, y: index % 2 === 0 ? -50 : 50 }}
+							animate={{ opacity: 1, x: 0, y: 0 }}
+							transition={{ duration: 0.4, delay: index * 0.1 }}
+							className="TechStackInfoText"
+							key={item}
+						>
+							{item}
+						</motion.div>
+					);
 				})}
 			</TechStackInfoContainer>
 		</MainContainer>
